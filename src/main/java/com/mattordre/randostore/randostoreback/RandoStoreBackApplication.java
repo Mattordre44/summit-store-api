6486231d6df8ec6b70c2c3aa531bd7a8937f567a
Matteo Ordrenneau <71778601+Mattordre44@users.noreply.github.com>
2024-11-22 15:06:41 +0100

    Initial commit

---
+++ src/main/java/com/mattordre/randostore/randostoreback/RandoStoreBackApplication.java
@@ -0,0 +1,13 @@
+package com.mattordre.randostore.randostoreback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandoStoreBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandoStoreBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mattordre/randostore/randostoreback/RandoStoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattordre.randostore.randostoreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandoStoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
